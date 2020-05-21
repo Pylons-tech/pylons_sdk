35eb7ea665025e56b2d11ce30b5d30b3a46be3d9
Eugen Steffen <stalepresh121@outlook.com>
2020-05-21 13:14:33 +1000

    ability to not create a new cookbook via fixture_test

--- cmd/fixtures_test/fixture_test.go
+++ cmd/fixtures_test/fixture_test.go
@@ -9,18 +9,18 @@ import (
 
 var runSerialMode bool = false
 var useRest bool = false
-var createNewCookbook bool = true
+var useKnownCookbook bool = false
 
 func init() {
 	flag.BoolVar(&runSerialMode, "runserial", false, "true/false value to check if test will be running in parallel")
 	flag.BoolVar(&useRest, "userest", false, "use rest endpoint for Tx send")
-	flag.BoolVar(&createNewCookbook, "newcookbook", true, "create new cookbook / update existing cookbook")
+	flag.BoolVar(&useKnownCookbook, "use-known-cookbook", false, "use existing cookbook or not")
 }
 
 func TestFixturesViaCLI(t *testing.T) {
 	flag.Parse()
 	FixtureTestOpts.IsParallel = !runSerialMode
-	FixtureTestOpts.CreateNewCookbook = createNewCookbook
+	FixtureTestOpts.CreateNewCookbook = !useKnownCookbook
 	if useRest {
 		intTest.CLIOpts.RestEndpoint = "http://localhost:1317"
 	}
